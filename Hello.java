7709a951adaba6c0c955f8f0769697b17d7b6a1f
BrAbysswalker <53832380+BrAbysswalker@users.noreply.github.com>
2020-04-29 12:57:51 -0300

    primeiro passo

---
+++ Hello.java
@@ -0,0 +1,10 @@
+ /**
+  Hello World
+  @author Flávio Ferreira de Souza
+  */
+
+  public class Hello {
+      public static void main(String[] args) {
+          System.out.println("Hello World");
+      }
+  }
